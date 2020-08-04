5685bb40b471f43d69f352183cc948838229ef70
rafaelmendes1 <rarafafa3245@gmail.com>
2020-08-03 21:56:08 -0300

    Consultando primeiro recurso com GET

--- src/main/java/com/example/algamoney/api/repository/CategoryRepository.java
+++ src/main/java/com/example/algamoney/api/repository/CategoryRepository.java
@@ -1,9 +1,10 @@
 package com.example.algamoney.api.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.algamoney.api.model.Category;
 
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long>{
 
 }
